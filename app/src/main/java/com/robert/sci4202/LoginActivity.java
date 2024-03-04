9cbaa74077155b18737839faf5d182a7707aa4ec
robert sibanda <robertsibanda20@gmail.com>
2024-03-04 23:21:57 +0200

    done permission settings

--- app/src/main/java/com/robert/sci4202/LoginActivity.java
+++ app/src/main/java/com/robert/sci4202/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.robert.sci4202;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            findViewById(R.id.btnLogin).setOnClickListener(l -> {
+                EditText etUsername =  findViewById(R.id.etUsername);
+                EditText etPassword = findViewById(R.id.etPassword);
+
+                if (etUsername.getText().length() > 3 && etPassword.getText().length() > 3) {
+
+                }
+                else {
+                    Toast.makeText(this,
+                            "Username and password must \nbe > 3 characters each",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
             return insets;
         });
     }
